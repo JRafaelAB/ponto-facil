79937269854dac26c8d3f9019d9ee5879982ba14
Jonas Rafael <jonasrafael97@hotmail.com>
2021-09-27 19:00:34 -0300

    Adiciona configurações de versionamento e configurações básicas do swagger

---
+++ src/WebApi/Modules/Swagger/SwaggerExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Modules.Swagger
+{
+    public static class SwaggerExtensions
+    {
+        public static IServiceCollection AddSwagger(this IServiceCollection services)
+        {
+            services.AddSwaggerGen();
+
+            return services;
+        }
+
+        public static IApplicationBuilder ConfigureSwagger(this IApplicationBuilder app, IApiVersionDescriptionProvider provider)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(option =>
+            {
+                foreach (var description in provider.ApiVersionDescriptions)
+                {
+                    option.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json", 
+                        description.GroupName.ToUpperInvariant());
+                }
+            });
+
+            return app;
+        }
+        
+    }
+}
---
+++ src/WebApi/Modules/VersioningExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApi.Modules
+{
+    public static class VersioningExtensions
+    {
+        public static IServiceCollection AddVersioning(this IServiceCollection services)
+        {
+            services
+                .AddApiVersioning(p =>
+                {
+                    p.DefaultApiVersion = new ApiVersion(1, 0);
+                    p.AssumeDefaultVersionWhenUnspecified = true;
+                });
+
+            services
+                .AddVersionedApiExplorer(p =>
+                {
+                    p.GroupNameFormat = "'v'VVV";
+                    p.SubstituteApiVersionInUrl = true;
+                });
+
+            return services;
+        }
+        
+    }
+}
