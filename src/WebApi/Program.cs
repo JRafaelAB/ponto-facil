afd4a8285457b439dbe22e7e2a6c439e6e3f28f8
Jonas Rafael <jonasrafael97@hotmail.com>
2021-09-29 18:47:17 -0300

    Adiciona configuração de logger

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace WebApi
 {
@@ -12,6 +13,14 @@ namespace WebApi
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((_, config) =>
+                {
+                    var settings = config.Build();
+                    Log.Logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(settings)
+                        .CreateLogger();
+                })
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
