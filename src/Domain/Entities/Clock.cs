7be0a39a88e47ab8f765fb49108235b91c233849
Jonas Rafael <jonasrafael97@hotmail.com>
2021-10-18 11:35:36 -0300

    Adiciona construtores às entidades

--- src/Domain/Entities/Clock.cs
+++ src/Domain/Entities/Clock.cs
@@ -6,9 +6,16 @@ namespace Domain.Entities
     public class Clock
     {
         public long Id { get; init; }
-        public ClockType Type { get; init; }
-        public DateTime Time { get; init; }
-        public long IdUser { get; init; }
+        public ClockType Type { get; }
+        public DateTime Time { get; }
+        public long IdUser { get; }
         public User? User { get; init; }
+        
+        public Clock(ClockType type, DateTime time, long idUser)
+        {
+            Type = type;
+            Time = time;
+            IdUser = idUser;
+        }
     }
 }
--- src/Domain/Entities/User.cs
+++ src/Domain/Entities/User.cs
@@ -5,16 +5,18 @@ namespace Domain.Entities
     public class User
     {
         public long Id { get; init; }
-        public string Name { get; init; }
-        public string Login { get; init; }
-        public string Password { get; init; }
-        public string Salt { get; init; }
+        public string Name { get; }
+        public string Login { get; }
+        public string Password { get; }
+        public string Salt { get; }
         public IEnumerable<Clock>? Clocks { get; init; }
-        public User(string name, string login, string password)
+        
+        public User(string name, string login, string password, string salt)
         {
             Name = name;
             Login = login;
             Password = password;
+            Salt = salt;
         }
     }
 }
