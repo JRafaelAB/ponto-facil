f8518b1e92dfdc591efed318f0c1f985e017ae0e
Jonas Rafael <jonasrafael97@hotmail.com>
2021-10-18 12:34:41 -0300

    Início do desenvolvimento do UserController.cs

---
+++ src/Infrastructure/Models/Requests/PostUserRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Infrastructure.Models.Requests
+{
+    public class PostUserRequest
+    {
+        public string Name { get; }
+        public string Login { get; }
+        public string Password { get; }
+        
+
+        public PostUserRequest(string name, string login, string password)
+        {
+            Name = name;
+            Login = login;
+            Password = password;
+        }
+    }
+}
--- src/WebApi/Controllers/v1/UseCases/PostUser/UserController.cs
+++ src/WebApi/Controllers/v1/UseCases/PostUser/UserController.cs
@@ -1,22 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace WebApi.Controllers.v1
+namespace WebApi.Controllers.v1.UseCases.PostUser
 {
     /// <summary>
-    /// HelloWorldController
+    /// UserController
     /// </summary>
     [ApiVersion("1.0")]
     [ApiController]
     [Route("v{version:apiVersion}/[controller]")]
-    public class HelloWorldController : ControllerBase
+    public class UserController : ControllerBase
     {
         /// <summary>
         /// Hello World
         /// </summary>
         /// <returns>A greeting message</returns>
         /// <response code="200">Returns the greeting message</response>
-        [HttpGet]
+        [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(string))]
         public IActionResult GetHelloWorld()
         {
