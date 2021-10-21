0c5e9667ed98c4080b4536f21b4d8d14ec03c80a
Jonas Rafael <jonasrafael97@hotmail.com>
2021-10-21 08:47:45 -0300

    Adiciona gerador de Salt e teste

---
+++ src/Domain/Utils/Salt.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace Domain.Utils
+{
+    public static class Salt
+    {
+        private static readonly Random _random = new ();
+        
+        public static string Generate(uint size)
+        {
+            StringBuilder str_build = new ();
+            
+            for (int i = 0; i < size; i++)
+            {
+                bool isNumber = _random.Next() % 2 == 0;
+                bool isUpperCase = _random.Next() % 2 != 0;
+                double seed = _random.NextDouble();
+                char value;
+                
+                if (isNumber)
+                {
+                    int shift = Convert.ToInt32(Math.Floor(10 * seed));
+                    value = Convert.ToChar(shift + 48);
+                }
+                else
+                {
+                    if (isUpperCase)
+                    {
+                        int shift = Convert.ToInt32(Math.Floor(25 * seed));
+                        value = Convert.ToChar(shift + 65);
+                    }
+                    else
+                    {
+                        int shift = Convert.ToInt32(Math.Floor(25 * seed));
+                        value = Convert.ToChar(shift + 97);
+                    }
+                }
+                str_build.Append(value);
+            }
+
+            return str_build.ToString();
+        }
+    }
+}
---
+++ tests/UnitTests/Utils/SaltUtilsTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using Domain.Utils;
+using Xunit;
+
+namespace UnitTests.Utils
+{
+    public class SaltUtilsTest
+    {
+        [Fact]
+        public void TestingSucess_Generate()
+        {
+            for (uint size = 0; size < 1000; size++)
+            {
+                string salt = Salt.Generate(size);
+                Assert.Equal((int)size, salt.Length);
+                foreach (char c in salt)
+                {
+                    Assert.Matches(new Regex("([a-z]|[0-9]|[A-Z])"), c.ToString());
+                }
+                
+            }
+        }
+    }
+}
