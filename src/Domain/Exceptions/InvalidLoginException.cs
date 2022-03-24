965ffe47f7cc65d88b6c49eb5a53b736d29906cc
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-03-24 17:06:26 -0300

    Adiciona exceção para login inválido

---
+++ src/Domain/Exceptions/InvalidLoginException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class InvalidLoginException : Exception
+    {
+        public NotificationError notificationError;
+
+        public InvalidLoginException(NotificationError notificationError)
+        {
+            this.notificationError = notificationError;
+        }
+    }
+}
