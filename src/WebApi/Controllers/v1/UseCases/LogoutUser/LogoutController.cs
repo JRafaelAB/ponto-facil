46b6d1981f4d1918c22360447b7a582564bc3e22
Anna Lopes <anna.lopes@dtidigital.com.br>
2022-05-16 21:48:12 -0300

    Adiciona controller para logout

---
+++ src/WebApi/Controllers/v1/UseCases/LogoutUser/LogoutController.cs
@@ -0,0 +1,32 @@
+﻿using Application.UseCases.LogoutUser;
+using Domain.Extensions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using WebApi.Controllers.Base;
+
+namespace WebApi.Controllers.v1.UseCases.LogoutUser
+{
+    /// <summary>
+    /// UserController
+    /// </summary>
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("v{version:apiVersion}/[controller]")]
+    public class LogoutController : BaseController
+    {
+        /// <summary>
+        /// Faz Logout.
+        /// </summary>
+        /// <response code="204">Successfull Request.</response>
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<IActionResult> LogoutAsync([FromServices] ILogoutUserUseCase useCase)
+        {
+            await useCase.Execute();
+
+            this.Response.Cookies.DeleteToken();
+            return NoContent();
+        }
+    }
+}
